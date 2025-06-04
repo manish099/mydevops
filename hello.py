2943186cf7cfda118d5e799465cb7d239c20b349
manish099 <visitatmanish@gmail.com>
2025-06-04 09:39:14 +0530

    Adding hello.py file

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello Devops Engineers, Welcome to the IQ India Organization!")
+
+# echo "# mydevops" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/manish099/mydevops.git
+# git push -u origin main
